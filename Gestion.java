fd421c49839335c064a53a0439e64b6b8137121c
Sansaur <arsansua@gmail.com>
2016-09-15 16:36:43 +0100

    Add files via upload

---
+++ Gestion.java
@@ -0,0 +1,10 @@
+
+/**
+ *
+ * @author Armando Sanchez Suarez
+ */
+public class Gestion {
+    public void Iniciar(){
+        System.out.println("El programa se ha iniciado y se va a acabar en nada porque eso es todo lo que hace");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+
+/**
+ *
+ * @author Armando Sanchez Suarez
+ * 
+ */
+public class Main {
+    public static void main(String[] args){
+        Gestion MiGestion = new Gestion();
+        MiGestion.Iniciar();
+    }
+}
